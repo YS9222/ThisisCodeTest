f1b6af49d74afb760793c155eef3aca76218f3c6
dovmf <dovmf@175.118.216.147>
2022-05-11 22:27:23 +0900

    implementation 4-1 retry (fail)

--- src/Implementation.java
+++ src/Implementation.java
@@ -1,7 +1,5 @@
-import java.util.Scanner;
 
 import java.util.*;
-
 //구현
 public class Implementation {
 
@@ -27,40 +25,77 @@ public class Implementation {
 		
 //		//해설 
 		//2차원없이 그냥 1,1에서 시작해서 입력받은대로 더하기 명령무시는 CONTINUE로
-        Scanner sc = new Scanner(System.in);
-
-        // N을 입력받기(전체지도규모) (BUT이걸로 전체규모를 먼저 지정하는게 아니라 이동좌표를 제한하는 정도의 수준역할)
-        int n = sc.nextInt();
-        sc.nextLine(); // 정수입력뒤 문자열입력위해 버퍼 비우기
-        String[] plans = sc.nextLine().split(" "); //입력받은걸 배열화 .SPLIT은 입력받은 걸 (입력)을 기준으로 분할함(여기서는 띄어쓰기)
-        int x = 1, y = 1; //최초좌표(초기화필요해서 1이 들어감)
-
-        // L, R, U, D에 따른 이동 방향 
-        int[] dx = {0, 0, -1, 1}; //x방향(direction x)
-        int[] dy = {-1, 1, 0, 0}; //y방향 //X,Y방향을 같은 배열에 넣어도 되지만 보기 좋게하기 위해서?
-        char[] moveTypes = {'L', 'R', 'U', 'D'}; //실제입력방향과 대조위한 역할
-
-        // 이동 계획을 하나씩 확인
-        for (int i = 0; i < plans.length; i++) {
-            char plan = plans[i].charAt(0);
-            // 이동 후 좌표 구하기 
-            int nx = -1, ny = -1; //아무입력이 없을시 아래 continue 작동시키기 위해서 -1?
-            for (int j = 0; j < 4; j++) { //반복문 통해 LRUD중 어느게 같은지 검증
-                if (plan == moveTypes[j]) { 
-                    nx = x + dx[j]; //이동할 좌표
-                    ny = y + dy[j];
-                }
-            }
-            // 이동할 좌표가 공간을 벗어나는 경우 이동할 좌표를 실제반영하지않고 무시 
-            if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
-            // 이동할 좌표대로 이동수행
-            x = nx; // 반복문이 반복되어야 하기 때문에 반복문에서 더함의 대상이 되는 x가 대상 
-            y = ny;
-        }
-
-        System.out.println(x + " " + y);
+//        Scanner sc = new Scanner(System.in);
+//
+//        // N을 입력받기(전체지도규모) (BUT이걸로 전체규모를 먼저 지정하는게 아니라 이동좌표를 제한하는 정도의 수준역할)
+//        int n = sc.nextInt();
+//        sc.nextLine(); // 정수입력뒤 문자열입력위해 버퍼 비우기
+//        String[] plans = sc.nextLine().split(" "); //입력받은걸 배열화 .SPLIT은 입력받은 걸 (입력)을 기준으로 분할함(여기서는 띄어쓰기)
+//        int x = 1, y = 1; //최초좌표(초기화필요해서 1이 들어감)
+//
+//        // L, R, U, D에 따른 이동 방향 
+//        int[] dx = {0, 0, -1, 1}; //x방향(direction x)
+//        int[] dy = {-1, 1, 0, 0}; //y방향 //X,Y방향을 같은 배열에 넣어도 되지만 보기 좋게하기 위해서?
+//        char[] moveTypes = {'L', 'R', 'U', 'D'}; //실제입력방향과 대조위한 역할
+//
+//        // 이동 계획을 하나씩 확인
+//        for (int i = 0; i < plans.length; i++) {
+//            char plan = plans[i].charAt(0);
+//            // 이동 후 좌표 구하기 
+//            int nx = -1, ny = -1; //아무입력이 없을시 아래 continue 작동시키기 위해서 -1?
+//            for (int j = 0; j < 4; j++) { //반복문 통해 LRUD중 어느게 같은지 검증
+//                if (plan == moveTypes[j]) { 
+//                    nx = x + dx[j]; //이동할 좌표
+//                    ny = y + dy[j];
+//                }
+//            }
+//            // 이동할 좌표가 공간을 벗어나는 경우 이동할 좌표를 실제반영하지않고 무시 
+//            if (nx < 1 || ny < 1 || nx > n || ny > n) continue;
+//            // 이동할 좌표대로 이동수행
+//            x = nx; // 반복문이 반복되어야 하기 때문에 반복문에서 더함의 대상이 되는 x가 대상 
+//            y = ny;
+//        }
+//
+//        System.out.println(x + " " + y);
 		
 
+//		Retry
+		Scanner sc = new Scanner(System.in);
+		int Size = sc.nextInt();
+		int x = 1;
+		int y = 1;
+		int Mx = 0;
+		int My = 0;
+		sc.nextLine();
+		String[] in = sc.nextLine().split(" ");
+		
+		char[] direction = {'L', 'R', 'U','D'};
+		int[] dx = {0,0,-1,1};
+		int[] dy = {-1,1,0,0};
+		
+		for(int i = 0; i<in.length; i++) {
+			char plan = in[i].charAt(0); //!!Error!! 이걸 빼먹음 String을 char으로 변환
+			
+			for(int j = 0; j<4; j++) {
+				if( direction[j] == plan) {
+					Mx = x+dx[j];
+					My = y+dy[j];
+				}
+			
+			}
+			if(Mx>Size || My>Size || Mx==0 || My==0 ) continue; //!!Error!! 아래 코드까지 위 반복문안에 넣었었음
+			
+			x =  Mx; //!!ERROR!! x = x + Mx 로 했었음 
+			y =  My;// Mx, My 자체가 이미 이동결과까지 반영하여 실질적으로 실행할지말지를 결정하는 단계인데 +=를 해버리면 
+					// 원위치 + (원위치+ 이동거리) 결과가 되어버림
+		}
+		System.out.println(x+" "+y);
+		
+		
+		
+		
+		
+		
 		
 	}
 
